6ef1572776207982fb06db4d4550e36730bd1c95
Daniels-Wright <deverdan@amazon.com>
2023-01-16 21:05:40 -0500

    First commit & cleanup

---
+++ src/pages/policies/privacy.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+export const PrivacyPolicyPage: NextPage = () => {
+  return (
+    <>
+      <h1>Privacy Policy</h1>
+    </>
+  );
+};
---
+++ src/pages/policies/tos.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+export const TermsOfServicePage: NextPage = () => {
+  return (
+    <>
+      <h1>Terms of Service</h1>
+    </>
+  );
+};
