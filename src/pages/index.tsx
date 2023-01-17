8e63a24251eaf8059cd3f53504791003b3c08f73
Everett Daniels-Wright <business.eadw@gmail.com>
2023-01-17 09:08:05 -0500

    Basic setup with some examples...

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import { type NextPage } from "next";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 
-import { api } from "../utils/api";
-
 const Home: NextPage = () => {
+  const { data: sessionData, status: sessionStatus } = useSession();
+
   return (
     <>
       <Head>
@@ -15,9 +15,16 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-slate-800 text-white">
-        <h1>Main</h1>
-        <AuthShowcase />
+      <main className="flex min-h-screen select-none flex-col items-center justify-center bg-slate-800 text-white">
+        <h1 className="text-2xl">
+          This page is being used for testing purposes...
+        </h1>
+        {sessionStatus === "authenticated" ? (
+          <p className="mt-3 font-semibold">
+            Your name is {sessionData.user?.name} and your email is{" "}
+            {sessionData.user?.email}...
+          </p>
+        ) : null}
       </main>
     </>
   );
@@ -25,28 +32,28 @@ const Home: NextPage = () => {
 
 export default Home;
 
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
+// const AuthShowcase: React.FC = () => {
+//   const { data: sessionData } = useSession();
 
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
+//   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+//     undefined, // no input
+//     { enabled: sessionData?.user !== undefined }
+//   );
 
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-        onClick={
-          sessionData ? () => void signOut() : () => void signIn("google")
-        }
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
+//   return (
+//     <div className="flex flex-col items-center justify-center gap-4">
+//       <p className="text-center text-2xl text-white">
+//         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+//         {secretMessage && <span> - {secretMessage}</span>}
+//       </p>
+//       <button
+//         className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+//         onClick={
+//           sessionData ? () => void signOut() : () => void signIn("google")
+//         }
+//       >
+//         {sessionData ? "Sign out" : "Sign in"}
+//       </button>
+//     </div>
+//   );
+// };
