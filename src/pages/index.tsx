a80c3b3e4125c6616d6bbc186a230c9a7b22faa5
Everett Daniels-Wright <business.eadw@gmail.com>
2023-01-22 03:30:27 -0500

    Updated home page and added loading based on session

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import { Spin } from "antd";
 import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
+import Link from "next/link";
 
 const HomePage: NextPage = () => {
+  const { status: sessionStatus } = useSession();
+
   return (
     <>
       <Head>
@@ -12,8 +17,47 @@ const HomePage: NextPage = () => {
         />
         <link rel="favicon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center">
-        <h1 className="text-2xl">This is the home page. You should log in.</h1>
+      <main className="flex min-h-screen flex-col items-center justify-center pl-10 pr-10">
+        {sessionStatus === "loading" ? (
+          <div className="flex h-96 flex-col justify-center">
+            <Spin size="large" />
+          </div>
+        ) : (
+          <div className="flex min-h-screen w-full max-w-6xl flex-col justify-center gap-10">
+            <p className="text-center">
+              Welcome to a D&amp;D tracker I made for myself and others. There
+              are a few simple tools and a nice &amp; fast searchable database
+              of a lot of game info. Enjoy!
+            </p>
+            <div className="flex w-full max-w-6xl flex-row items-center justify-center gap-10">
+              <Link className="w-full" href="/game-info">
+                <div className="home-screen-card game-rules-home-screen-card">
+                  <h2 className="text-4xl font-bold text-white drop-shadow-lg">
+                    Game Info
+                  </h2>
+                </div>
+              </Link>
+              {sessionStatus === "authenticated" ? (
+                <>
+                  <Link className="w-full" href="/characters">
+                    <div className="home-screen-card">
+                      <h2 className="text-4xl font-bold text-white drop-shadow-lg">
+                        Characters
+                      </h2>
+                    </div>
+                  </Link>
+                  <Link className="w-full" href="/campaigns">
+                    <div className="home-screen-card">
+                      <h2 className="text-4xl font-bold text-white drop-shadow-lg">
+                        Campaigns
+                      </h2>
+                    </div>
+                  </Link>
+                </>
+              ) : null}
+            </div>
+          </div>
+        )}
       </main>
     </>
   );
